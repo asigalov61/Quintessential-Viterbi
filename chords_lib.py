98f1291bcc6c044cfac00e3c58d1b661811e990b
Alex <asigalov61@hotmail.com>
2020-12-18 15:27:56 -0800

    Minor adjustment of code for simulated velocity.

--- chords_lib.py
+++ chords_lib.py
@@ -431,7 +431,7 @@ class BasicChordRenderer(ChordRenderer):
       note.start_time = start_time
       note.end_time = end_time
       note.pitch = pitch
-      note.velocity = 127 - pitch # self._velocity
+      note.velocity = pitch # self._velocity
       note.instrument = self._instrument
       note.program = self._program
 
